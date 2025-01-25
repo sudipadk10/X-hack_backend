47708c2a2d23b797b2976823614f71aa176bccbe
sudipadk10 <adhikarisudip869@gmail.com>
2025-01-25 13:16:50 +0545

    increase jwt expiry time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { jwtConstants } from './constants';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '2h' },
+      signOptions: { expiresIn: '2d' },
     }),
   ],
   providers: [AuthService],
