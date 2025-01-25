c290fb96ccaa7ffd7b654f0f7ac31566e8368ffa
sudipadk10 <adhikarisudip869@gmail.com>
2025-01-25 20:41:40 +0545

    fix error

--- src/booking/dto/update-booking-status.dto.ts
+++ src/booking/dto/update-booking-status.dto.ts
@@ -1,12 +1,6 @@
-import { IsEnum } from "class-validator";
-enum BookingStatus {
-    PENDING,
-    CONFIRMED,
-    COMPLETED,
-    CANCELLED
-  }
+
 
 export class UpdateBookingStatusDto {
-    @IsEnum(BookingStatus)
-    status: BookingStatus;
+    
+    status: "COMPLETED" | "CONFIRMED" |"COMPLETED" |"CANCELLED"
   }
